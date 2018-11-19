084826684b77f8444a0f9d5b432a305db183bc89
OopsL <xcoder_wk@hotmail.com>
2018-11-19 11:41:45 +0800

    ...

---
+++ checkBalance.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./run getBalance --address 张三
+./run getBalance --address 李四
+./run getBalance --address 王五
+./run getBalance --address 赵六
+
---
+++ transfer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./run send 张三 李四 10 Jack "张三转李四10"
+./run send 张三 王五 20 Jack "张三转王五20"
+
+./run send 王五 李四 2 Jack "王五转李四2"
+./run send 王五 李四 3 Jack "王五转李四3"
+./run send 王五 张三 5 Jack "王五转张三5"
+
+./run send 李四 赵六 14 Jack "李四转赵六14"
+
